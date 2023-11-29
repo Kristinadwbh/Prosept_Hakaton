b757a0a609de18de433d458841baf9d18f673e62
Серей Овинцев <ozonti@mail.ru>
2023-11-29 13:04:09 +0300

    Форматирование к стандарту PEP8

--- example.py
+++ example.py
@@ -11,14 +11,27 @@ df_res = pd.DataFrame(res)
 df_res.head()
 
 
-test = tst_mdp.merge(mpdk, how='left', left_on='product_key',
-               right_on='key').loc[:, ['product_key', 'key', 'product_id']]
+test = tst_mdp.merge(mpdk,
+                     how='left',
+                     left_on='product_key',
+                     right_on='key').loc[:, [
+                                                'product_key',
+                                                'key',
+                                                'product_id'
+                                                   ]
+       ]
+
 test = pd.concat([test, df_res], axis=1)
+
+
 def recall_k(k):
     for i in range(test.shape[0]):
-        if test.loc[i, 'product_id'] in test.loc[i, [str(t) for t in range(1, k+1)]].values:
+        if test.loc[i, 'product_id'] in test.loc[
+            i, [str(t) for t in range(1, k + 1)]].values:
             test.loc[i, 'recall@5'] = 1
         else:
             test.loc[i, 'recall@5'] = 0
+
+
 recall_k(5)
-print('Accuracy : ', sum(test['recall@5'])/test.shape[0])
+print('Accuracy : ', sum(test['recall@5']) / test.shape[0])
